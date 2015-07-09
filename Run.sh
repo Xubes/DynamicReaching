fede582bc12c05c23fd89c805a0a0f9f87033ea7
Shohan Hasan <shohan.hasan@nyu.edu>
2015-07-09 15:30:39 -0400

    Fix Run script.

--- Run.sh
+++ Run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 actionbot="${HOME}/actionbot/src/actionbot"
-dynreach="${HOME}~/DynamicReaching/application.linux64/DynamicReaching"
-pportL=`ls /dev | egrep "cu.usbmodem\d+"`
-portR="/dev/cu.usbmodemRTQ0011"
-portC="/dev/cu.usbmodemfd1211"
+dynreach="${HOME}/DynamicReaching/application.linux64/DynamicReaching"
+portL=`ls /dev | egrep "cu.usbmodem\d+"`
+portR='/dev/cu.usbmodemRTQ0011'
+portC='/dev/cu.usbmodemfd1211'
 
-"$dynreach" | "$actionbot" $portL $portR $portC
+"$dynreach" | "$actionbot" "/dev/${portL}" "$portR" "$portC"
