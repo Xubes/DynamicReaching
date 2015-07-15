78f295acd34ed5babe5985c89bd68a407d2458b7
Shohan Hasan <shohan.hasan@nyu.edu>
2015-07-15 14:04:52 -0400

    Update Run script.

--- Run.sh
+++ Run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 actionbot="${HOME}/actionbot/src/actionbot"
-dynreach="${HOME}/DynamicReaching/application.linux64/DynamicReaching"
+dynreach="${HOME}/Documents/Processing/DynamicReaching/application.linux64/DynamicReaching"
 portL=`ls /dev | egrep "cu.usbmodem\d+"`
 portR='/dev/cu.usbmodemRTQ0011'
 portC='/dev/cu.usbmodemfd1211'
