5f0b2b8bbdc8224c46943ed6b833a51823889df7
Shohan Hasan <shohan.hasan@nyu.edu>
2016-06-02 15:12:27 -0400

    Update run script.

--- Run.sh
+++ Run.sh
@@ -2,10 +2,10 @@
 
 robocommander="${HOME}/Documents/workspace/robocommander/Release/robocommander"
 dynreach="${HOME}/Documents/Processing/DynamicReaching/application.linux64/DynamicReaching"
-portL="/dev/`ls /dev | egrep 'cu.usbmodem\d+'`"
+#portL="/dev/`ls /dev | egrep 'cu.usbmodem\d+'`"
 portL=' '
 #portR='/dev/cu.usbmodemRTQ0011'
 portR=' '
 portC='/dev/cu.usbmodemfd1241'
 
-"$dynreach" | "$robocommander" "/dev/${portL}" "$portR" "$portC"
+"$dynreach" | "$robocommander" "$portL" "$portR" "$portC"
